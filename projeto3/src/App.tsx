bea5c5ecc6ed575029ca8f5334209376bdf507a7
PedroHaratani <76952955+PedroHaratani@users.noreply.github.com>
2022-10-03 23:53:05 -0300

    Criada a tabela e ajustado as datas

--- projeto3/src/App.tsx
+++ projeto3/src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react'
+import * as C from './App.styles';
+import {Item} from './types/Item';
+import {Category} from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/item';
+import {getCurrentMonth,FilterListListByMonth} from './helpers/dateFilter';
+import {TableArea} from './components/TableArea';
 
-function App() {
+export const App = () =>{
+  const [list,setList] = useState(items);
+  
+  const [filteredList,setFilteredList] = useState<Item[]>([]);
+
+  const [currentMonth,setCurrentMonth] = useState(getCurrentMonth());
+  
+  useEffect(()=>{
+    setFilteredList( FilterListListByMonth(list,currentMonth) );
+  },[list,currentMonth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/*Area de iformações */}
+
+        {/*Area de inserção */}
+
+        <TableArea list={filteredList}/>
+        
+        ...
+      </C.Body>
+    </C.Container>
   );
 }
 
---
+++ projeto3/src/data/item.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item';
+
+export const items:Item[] = [
+  { date:new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value:32.12 },
+  { date:new Date(2022, 9, 15), category: 'food', title: 'Burguer King', value:28 },
+  { date:new Date(2022, 9, 15), category: 'aluguel', title: 'Aluguel', value:2300 },
+  { date:new Date(2022, 10, 15), category: 'salary', title: 'Salario', value:4500 },
+];
